7f2383f2ef6004066c48357667ed4d5a27f91784
Jonathan Windell <160614774+JonathanWindell@users.noreply.github.com>
2026-03-28 20:21:57 +0100

    Clean up comments in RunCucumberTest.java
    Removed Swedish comments from RunCucumberTest.java.

--- src/test/java/logger/runners/RunCucumberTest.java
+++ src/test/java/logger/runners/RunCucumberTest.java
@@ -8,9 +8,7 @@ import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
-// Denna pekar på mappen i src/test/resources där dina .feature-filer ligger
 @SelectClasspathResource("features")
-// Denna berättar var dina steg-definitioner (Java-filer) ligger
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "logger.steps")
 public class RunCucumberTest {
   // Denna klass ska vara tom, den fungerar bara som en startknapp
